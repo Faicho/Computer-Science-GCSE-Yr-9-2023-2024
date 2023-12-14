f0bf0813dea97d7d4fea72b9efd22869557e7f2b
faiyaz <faiyazchoudhury123@hotmail.com>
2023-12-14 20:41:30 +0000

    comittedf changes

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+def get_month(n):
+    print({1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}[n])#or.get(n)
+
+get_month(4)
